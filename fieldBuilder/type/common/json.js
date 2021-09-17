6da6e36c4c189d6af81a0bfaa8485032c3e8a549
zarvat <wdrums56@gmail.com>
2021-09-17 09:42:48 +0300

    JSON type without validation

--- fieldBuilder/type/common/json.js
+++ fieldBuilder/type/common/json.js
@@ -2,18 +2,52 @@ import { QIcon, QInput } from 'quasar'
 import tooltip from '../../components/tooltip'
 
 export default function ({ fieldInitData, additionalFieldProps }) {
+  const rawValue = fieldInitData.props.value
+  const value = typeof rawValue === 'object' ? JSON.stringify(rawValue, null, 2) : rawValue
+  let isNotJSON = false
+
   return {
     name: 'json',
     component: QInput,
+    data: () => {
+      return {
+        value: ''
+      }
+    },
     on: {
-      change: fieldInitData.onChange,
+      change: (event) => {
+        const value = event.srcElement.value
+        try {
+          JSON.parse(value);
+        } catch (e) {
+          return
+        }
+
+        fieldInitData.onChange(value, {
+          clearValue: (value) => {
+            return JSON.parse(value)
+          }
+        })
+      },
+      input: fieldInitData.inputDebounce(value => {
+        let parsedValue
+        try {
+          parsedValue = JSON.parse(value);
+        } catch (e) {
+          isNotJSON = true
+        }
+        const newValue = parsedValue ? JSON.stringify(parsedValue, null, 2) : value
+        fieldInitData.onInput(newValue, null, null, true, true)
+      }),
     },
     props: {
       filled: false,
       outlined: true,
       disabled: true,
       readonly: false,
-      type: 'textarea'
+      type: 'textarea',
+      'bottom-slots': true,
+      value: value
     },
     scopedSlots: {
       after: () => {
