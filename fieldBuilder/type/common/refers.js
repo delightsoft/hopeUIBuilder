c9add420c8b90b4ad8bb0d071fcf1cd79051b555
zarvat <wdrums56@gmail.com>
2021-09-20 15:10:59 +0300

    refers for multiple typed field

--- fieldBuilder/type/common/refers.js
+++ fieldBuilder/type/common/refers.js
@@ -1,13 +1,16 @@
-import readonly from '../../components/readonly'
 import Vue from 'vue'
 import APICommon from '../../../../APICommon'
 import Multiselect from 'vue-multiselect'
 import 'vue-multiselect/dist/vue-multiselect.min.css';
-import errorWrapper from '../../components/errorWrapper.vue'
+import { QOptionGroup } from "quasar";
 
 export default function ({ fieldInitData, additionalFieldProps }) {
 
-  const fillFilteredOptions = async ({ search, pageNo, pageSize = 3 }) => {
+  const getType = () => {
+    return fieldInitData.model[fieldInitData.fieldName]?._type || fieldInitData.field.refers[0].$$key
+  }
+
+  const fillFilteredOptions = async ({ search, pageNo, pageSize = 7, docType }) => {
     Vue.set(this.uiModel[fieldInitData.fieldName], 'isLoading', true);
 
     const filter = {}
@@ -24,7 +27,7 @@ export default function ({ fieldInitData, additionalFieldProps }) {
       service: 'hope',
       method: 'list',
       args: {
-        type: fieldInitData.field.refers[0].$$key,
+        type: docType,
         filter,
         pageNo,
         pageSize,
@@ -67,155 +70,205 @@ export default function ({ fieldInitData, additionalFieldProps }) {
 
   let propPlaceholder = additionalFieldProps.propPlaceholder || fieldInitData.props.label || 'Select option';
 
-  return {
-    name: 'refers',
-    component: errorWrapper,
-    readonlyComponent: readonly,
-    props: {
-      error: fieldInitData.props.error,
-      'error-message': fieldInitData.props['error-message'],
-      hint: fieldInitData.props.hint,
-      readonlyWithoutDefaultSlot: true
-    },
-    scopedSlots: {
-      default: () => this.h(
-        Multiselect,
-        {
-          name: 'refers',
-          ref,
-          on: {
-            'search-change': async (val) => {
-              if (!val.trim().length) {
-                Vue.set(this.uiModel[fieldInitData.fieldName], 'isLoading', false);
-                return
-              }
-              fillFilteredOptions({ search: val.toLowerCase(), pageNo: 1 })
-            },
-            open: async (val) => {
-              fillFilteredOptions({ pageNo: 1 })
-            },
-            input: (value) => {
-              fieldInitData.onInput(value)
-              Object.assign(fieldInitData.props, { value })
-            },
-            close: (value, id) => {
-              this.$set(this.uiModel[fieldInitData.fieldName], 'filteredOptions', ['hues']);
-            },
+  const refers = () => {
+    return this.h(
+      Multiselect,
+      {
+        name: 'refSelect',
+        ref,
+        on: {
+          'search-change': async (val) => {
+            if (!val.trim().length) {
+              Vue.set(this.uiModel[fieldInitData.fieldName], 'isLoading', false);
+              return
+            }
+            fillFilteredOptions({
+              search: val.toLowerCase(),
+              pageNo: 1,
+              docType: getType()
+            })
           },
-          props: {
-            placeholder: propPlaceholder,
-            'hide-selected': false,
-            'internal-search': true,
-            label: 'label',
-            'options-limit': 20,
-            taggable: additionalFieldProps.taggable,
-            closeOnSelect: true,
-            loading: this.uiModel[fieldInitData.fieldName].isLoading,
-            options: this.uiModel[fieldInitData.fieldName].filteredOptions || [],
-            internalSearch: false,
-            allowEmpty: !!additionalFieldProps.multiple,
-            deselectLabel: !!additionalFieldProps.multiple ? 'Press enter to remove' : '',
-
-            value: fieldInitData.props.value,
-            // error: fieldInitData.props.error,
-            // 'error-message': fieldInitData.props['error-message'],
-            // tabindex: fieldInitData.tabindex,
+          open: async (val) => {
+            fillFilteredOptions({ pageNo: 1, docType: getType() })
           },
-          scopedSlots: {
-            noOptions: () => {
-              return 'Start typing to search'
-            },
-            singleLabel: (value) => {
-              const val = value.option
-              if (val && val.label) {
-                return this.h(
-                  'div',
-                  {
-                    style: {
-                      display: 'flex'
-                    }
-                  },
-                  [
-                    this.h(
-                      'div',
-                      {
-                        style: {
-                          marginBottom: '-3px',
-                          marginRight: '4px',
-                          padding: '4px 10px 4px 10px',
-                          lineHeight: '14px',
-                        },
-                        class: ['multiselect__tag']
-                      },
-                      val.label
-                    ),
-                  ]
-                )
-              }
-
-              return this.h(
-                'div',
-                {
-                  style: {
-                    display: 'flex',
-                    color: '#adadad',
-                  },
-                },
-                [propPlaceholder, fieldInitData.props.required ? requiredElement : '']
-              )
-            },
-            placeholder: (value) => {
+          input: (value) => {
+            fieldInitData.onInput(value)
+            Object.assign(fieldInitData.props, { value })
+          },
+          close: (value, id) => {
+            this.$set(this.uiModel[fieldInitData.fieldName], 'filteredOptions', ['hues']);
+          },
+        },
+        props: {
+          placeholder: propPlaceholder,
+          'hide-selected': false,
+          'internal-search': true,
+          label: 'label',
+          'options-limit': 20,
+          taggable: additionalFieldProps.taggable,
+          closeOnSelect: true,
+          loading: this.uiModel[fieldInitData.fieldName].isLoading,
+          options: this.uiModel[fieldInitData.fieldName].filteredOptions || [],
+          internalSearch: false,
+          allowEmpty: !!additionalFieldProps.multiple,
+          deselectLabel: !!additionalFieldProps.multiple ? 'Press enter to remove' : '',
+          disable: !!getType(),
+          value: fieldInitData.props.value,
+        },
+        scopedSlots: {
+          noOptions: () => {
+            return 'Start typing to search'
+          },
+          singleLabel: (value) => {
+            const val = value.option
+            if (val && val.label) {
               return this.h(
                 'div',
                 {
                   style: {
                     display: 'flex'
-                  },
-                },
-                [propPlaceholder, fieldInitData.props.required ? requiredElement : '']
-              )
-            },
-            afterList: (value) => {
-              if (this.uiModel[fieldInitData.fieldName].isLast) {
-                return
-              }
-              return this.h(
-                'button',
-                {
-                  style: {
-                    width: '100%',
-                  },
-                  on: {
-                    click: async () => {
-                      fillFilteredOptions({ pageNo: ++this.uiModel[fieldInitData.fieldName].pageNo })
-                    }
                   }
                 },
-                '>>'
-              )
-            },
-            beforeList: (value) => {
-              if (this.uiModel[fieldInitData.fieldName].pageNo === 1) {
-                return
-              }
-              return this.h(
-                'button',
-                {
-                  style: {
-                    width: '100%',
-                  },
-                  on: {
-                    click: async () => {
-                      fillFilteredOptions({ pageNo: --this.uiModel[fieldInitData.fieldName].pageNo })
-                    }
-                  }
-                },
-                '<<'
+                [
+                  this.h(
+                    'div',
+                    {
+                      style: {
+                        marginBottom: '-3px',
+                        marginRight: '4px',
+                        padding: '4px 10px 4px 10px',
+                        lineHeight: '14px',
+                      },
+                      class: ['multiselect__tag']
+                    },
+                    val.label
+                  ),
+                ]
               )
             }
+
+            return this.h(
+              'div',
+              {
+                style: {
+                  display: 'flex',
+                  color: '#adadad',
+                },
+              },
+              [propPlaceholder, fieldInitData.props.required ? requiredElement : '']
+            )
+          },
+          placeholder: (value) => {
+            return this.h(
+              'div',
+              {
+                style: {
+                  display: 'flex'
+                },
+              },
+              [propPlaceholder, fieldInitData.props.required ? requiredElement : '']
+            )
+          },
+          afterList: (value) => {
+            if (this.uiModel[fieldInitData.fieldName].isLast) {
+              return
+            }
+            return this.h(
+              'button',
+              {
+                style: {
+                  width: '100%',
+                },
+                on: {
+                  click: async () => {
+                    fillFilteredOptions({
+                      pageNo: ++this.uiModel[fieldInitData.fieldName].pageNo,
+                      docType: getType()
+                    })
+                  }
+                }
+              },
+              '>>'
+            )
+          },
+          beforeList: (value) => {
+            if (this.uiModel[fieldInitData.fieldName].pageNo === 1) {
+              return
+            }
+            return this.h(
+              'button',
+              {
+                style: {
+                  width: '100%',
+                },
+                on: {
+                  click: async () => {
+                    fillFilteredOptions({
+                      pageNo: --this.uiModel[fieldInitData.fieldName].pageNo,
+                      docType: getType()
+                    })
+                  }
+                }
+              },
+              '<<'
+            )
           }
         }
-      ),
-    }
+      }
+    )
+  }
+
+  const optionGroup = () => {
+    return !(fieldInitData.field.refers.length === 1) && this.h(
+      QOptionGroup,
+      {
+        name: 'refOptions',
+        ref,
+        on: {
+          input: (value) => {
+            fieldInitData.model[fieldInitData.fieldName]._type = value
+          },
+        },
+        props: {
+          label: 'label',
+          options: fieldInitData.field.refers.map(item => {
+            return {
+              label: item.$$key,
+              value: item.$$key
+            }
+          }),
+          value: getType(),
+        },
+      }
+    )
+  }
+
+  return {
+    name: 'refers',
+    component: 'div',
+    col: {
+      xs: 12,
+      sm: 12,
+      md: 12,
+      lg: 12,
+      xl: 12,
+    },
+    props: {
+      filled: true,
+    },
+    directives: [
+      {
+        name: 'l',
+        value: {
+          [`${fieldInitData.field.$$key}.label`]: this.$t(`${fieldInitData.field.$$key}.label`),
+          [`${fieldInitData.field.$$key}.subtitle`]: this.$t(`${fieldInitData.field.$$key}.subtitle`),
+          [`${fieldInitData.field.$$key}.title {index}`]: this.$t(`${fieldInitData.field.$$key}.title`),
+        },
+      }
+    ],
+    children: [
+      optionGroup(),
+      refers()
+    ],
   }
 }
