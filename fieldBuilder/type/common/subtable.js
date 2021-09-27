9c87ea173b381f8faeee7016fc7f084074556c5e
zarvat <wdrums56@gmail.com>
2021-09-27 10:10:25 +0300

    draggable subtable

--- fieldBuilder/type/common/subtable.js
+++ fieldBuilder/type/common/subtable.js
@@ -1,5 +1,6 @@
 import { QBtn } from 'quasar'
 import readonly from '../../components/readonly/index.vue'
+import draggable from 'vuedraggable'
 
 export default function ({ fieldInitData, additionalFieldProps }) {
   if (!fieldInitData.props.value) {
@@ -8,9 +9,130 @@ export default function ({ fieldInitData, additionalFieldProps }) {
       component: 'div',
     }
   }
-  return {
+
+  let isDragging = false
+
+  const fieldChildren = fieldInitData.model[fieldInitData.fieldName].map((model, index) => this.h(
+    'div',
+    {
+      class: 'array-item col-12',
+    },
+    [
+      !additionalFieldProps.sectionUntitled && this.h(
+        'h3',
+        {
+          class: 'text-h6',
+        },
+        `${index + 1}.` + this.$t(this.$t(`${fieldInitData.field.$$key}.label`, { index: index + 1 }), { index: index + 1 })
+      ),
+      this.h(
+        'div',
+        {
+          class: 'row q-col-gutter-md',
+        },
+        [
+          ...this.getChildren({
+            h: this.h,
+            fields: fieldInitData.field.fields.$$list,
+            parent: {
+              name: [...fieldInitData.parent.name || [], fieldInitData.fieldName],
+              index: [...fieldInitData.parent.index || [], index],
+            },
+          }),
+          !fieldInitData.props.readonly && this.h(
+            'div',
+            {
+              class: 'col-auto',
+            },
+            [
+              this.h(
+                QBtn,
+                {
+                  class: 'big-element',
+                  props: {
+                    color: 'primary',
+                    outline: true,
+                    unelevated: true,
+                    tabindex: fieldInitData.tabindex,
+                  },
+                  on: {
+                    click: () => fieldInitData.deleteElementFromArray(index),
+                  },
+                  directives: [
+                    {
+                      name: 'l',
+                      value: {
+                        [`${fieldInitData.field.$$key}.button.delete.label`]: this.$t(`${fieldInitData.field.$$key}.button.delete.label`),
+                      },
+                    }
+                  ],
+                },
+                this.$t(this.$t(`${fieldInitData.field.$$key}.button.delete.label`)),
+              ),
+            ]
+          ),
+        ]
+      )
+    ]
+  ))
+
+  const children = [
+    // !additionalFieldProps.untitled && this.h(
+    //   'div',
+    //   [
+    //     this.h(
+    //       'h3',
+    //       {
+    //         class: 'col-12 text-h5',
+    //         style: 'margin-top: 0px; margin-bottom: 4px;',
+    //       },
+    //       this.$t(this.$t(`${fieldInitData.field.$$key}.label`)),
+    //     ),
+    //     !!fieldInitData.model[fieldInitData.fieldName]?.length || this.h(
+    //       'p',
+    //       {
+    //         class: `col-12`,
+    //         style: 'margin-bottom: 0;',
+    //       }, [
+    //         this.$t(this.$t(`${fieldInitData.field.$$key}.subtitle`)),
+    //       ]
+    //     ),
+    //   ],
+    // ),
+    ...fieldChildren,
+
+    !fieldInitData.props.readonly && (fieldInitData.model[fieldInitData.fieldName]?.length < additionalFieldProps.elementsLimit || !additionalFieldProps.elementsLimit) ?
+      this.h(
+        QBtn,
+        {
+          class: 'col-12 big-element',
+          style: 'margin-bottom: 20px; margin-top: 20px;',
+          props: {
+            color: 'primary',
+            outline: true,
+            unelevated: true,
+            tabindex: fieldInitData.tabindex,
+          },
+          on: {
+            click: fieldInitData.addElementInArray,
+          },
+          directives: [
+            {
+              name: 'l',
+              value: {
+                [`${fieldInitData.field.$$key}.action.create.label`]: this.$t(`${fieldInitData.field.$$key}.action.create.label`),
+              },
+            }
+          ],
+        },
+        this.$t(this.$t(`${fieldInitData.field.$$key}.action.create.label`)),
+      )
+      : undefined
+  ]
+
+  const subtable = {
     name: 'subtable',
-    component: 'div',
+    component: draggable,
     readonlyComponent: fieldInitData.props.value.length > 0 ? undefined : readonly,
     col: {
       xs: 12,
@@ -19,8 +141,24 @@ export default function ({ fieldInitData, additionalFieldProps }) {
       lg: 12,
       xl: 12,
     },
+    on: {
+      start: () => {
+        isDragging = true
+      },
+      end: () => {
+        isDragging = false
+      },
+      input: fieldInitData.inputDebounce(value => {
+        fieldInitData.onInput(value)
+      }),
+    },
     props: {
       filled: true,
+      move: ({ relatedContext, draggedContext }) => {
+        const relatedElement = relatedContext.element;
+        const draggedElement = draggedContext.element;
+        return draggedElement && relatedElement;
+      }
     },
     directives: [
       {
@@ -32,120 +170,8 @@ export default function ({ fieldInitData, additionalFieldProps }) {
         },
       }
     ],
-    children: [
-      !additionalFieldProps.untitled && this.h(
-        'div',
-        [
-          this.h(
-            'h3',
-            {
-              class: 'col-12 text-h5',
-              style: 'margin-top: 0px; margin-bottom: 4px;',
-            },
-            this.$t(this.$t(`${fieldInitData.field.$$key}.label`)),
-          ),
-          !!fieldInitData.model[fieldInitData.fieldName]?.length || this.h(
-            'p',
-            {
-              class: `col-12`,
-              style: 'margin-bottom: 0;',
-            }, [
-              this.$t(this.$t(`${fieldInitData.field.$$key}.subtitle`)),
-            ]
-          ),
-        ],
-      ),
-      ...fieldInitData.model[fieldInitData.fieldName].map((model, index) => this.h(
-        'div',
-        {
-          class: 'array-item col-12',
-        },
-        [
-          !additionalFieldProps.sectionUntitled && this.h(
-            'h3',
-            {
-              class: 'text-h6',
-            },
-            `${index + 1}.` + this.$t(this.$t(`${fieldInitData.field.$$key}.label`, { index: index + 1 }), { index: index + 1 })
-          ),
-          this.h(
-            'div',
-            {
-              class: 'row q-col-gutter-md',
-            },
-            [
-              ...this.getChildren({
-                h: this.h,
-                fields: fieldInitData.field.fields.$$list,
-                parent: {
-                  name: [...fieldInitData.parent.name || [], fieldInitData.fieldName],
-                  index: [...fieldInitData.parent.index || [], index],
-                },
-              }),
-              !fieldInitData.props.readonly && this.h(
-                'div',
-                {
-                  class: 'col-auto',
-                },
-                [
-                  this.h(
-                    QBtn,
-                    {
-                      class: 'big-element',
-                      props: {
-                        color: 'primary',
-                        outline: true,
-                        unelevated: true,
-                        tabindex: fieldInitData.tabindex,
-                      },
-                      on: {
-                        click: () => fieldInitData.deleteElementFromArray(index),
-                      },
-                      directives: [
-                        {
-                          name: 'l',
-                          value: {
-                            [`${fieldInitData.field.$$key}.button.delete.label`]: this.$t(`${fieldInitData.field.$$key}.button.delete.label`),
-                          },
-                        }
-                      ],
-                    },
-                    this.$t(this.$t(`${fieldInitData.field.$$key}.button.delete.label`)),
-                  ),
-                ]
-              ),
-            ]
-          )
-        ]
-      )),
-
-      !fieldInitData.props.readonly && (fieldInitData.model[fieldInitData.fieldName]?.length < additionalFieldProps.elementsLimit || !additionalFieldProps.elementsLimit) ?
-        this.h(
-          QBtn,
-          {
-            class: 'col-12 big-element',
-            style: 'margin-bottom: 20px; margin-top: 20px;',
-            props: {
-              color: 'primary',
-              outline: true,
-              unelevated: true,
-              tabindex: fieldInitData.tabindex,
-            },
-            on: {
-              click: fieldInitData.addElementInArray,
-            },
-            directives: [
-              {
-                name: 'l',
-                value: {
-                  [`${fieldInitData.field.$$key}.action.create.label`]: this.$t(`${fieldInitData.field.$$key}.action.create.label`),
-                },
-              }
-            ],
-          },
-          this.$t(this.$t(`${fieldInitData.field.$$key}.action.create.label`)),
-        )
-        : undefined
-    ]
+    children: children
   }
+
+  return subtable
 }
